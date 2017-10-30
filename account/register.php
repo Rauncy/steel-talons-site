f5d4e249e7cbfe496d252bff08e695a585213461
OTHSCS153 <OTHSCS153@KATYISD.ORG>
2017-10-30 13:34:37 -0500

    Added database support and some redirect features

--- account/register.php
+++ account/register.php
@@ -1,13 +1,12 @@
 <?php $dir = ".."; include($dir . "/header.php"); ?>
 <link rel = "stylesheet"  href = "/css/login.css">
+<script src = "/js/account.js"></script>
 <div id="content">
-  <form class="logIn" method="post">
+  <form class="logIn" onsubmit="register();" action="/index.php/register" method="post">
       <p class = "labelLog" id = "email">E - Mail: <input type = "text" id = "registerEmail" placeholder="johnappleseed@gmail.com"></p>
       <p class = "labelLog" >Password: <input type="password" id= "registerPassword" placeholder="******"></p>
       <p class = "labelLog" id = "confirm">Confirm Password: <input type="password" id="confirmPassword" placeholder="******"></p>
-      <button id = "submit" onclick = "register()">
-        <img src = "https://openclipart.org/image/800px/svg_to_png/191072/Blue-Robot.png" height = "50"; width="50" style="float:left;margin-right:0.5em">
-      </button>
+      <input type="submit" value = "Register"></input>
       <br>
       <br>
 
