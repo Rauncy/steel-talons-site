e0cf7f03fa33ccd44f3808a6354e5247ae823828
Blake Romero <blake.romero724@gmail.com>
2017-10-31 13:18:04 -0500

    Changed 404 page.

--- 404.php
+++ 404.php
@@ -1,22 +1,13 @@
 <!DOCTYPE HTML>
 <html>
-  <head>
-    <title><big>404</big></title>
-  </head>
-  <body style =" background-color: #121c59;">
-    <center><h1 style="border:dotted; background-color: YELLOW"><big><big><big><big><big><big><big><big>404 ERROR</big></big></big></big></big></big></big></big></h1></center>
-    <center>
-    <p style="color: white"><big><big><big><big>=========================================================</big></big></big></big></p>
-    <p style="color: white"><big><big><big><big>You have entered an unknown part of the robotic dimension!</big></big></big></big></p>
-    <p style="color: white"><big><big><big><big>=========================================================</big></big></big></big></p>
-    <h2 style="color: white"><strong><b>Quick Return to the Safe Zone!<b></strong></h2>
-
-      <center><img src="BRobot.jpg" alt="Broken Robot" style="width:304px;height:228px;"></center>
-
-      <a href="/">Return to the homepage</a>
-
-
-  </center>
-
-  </body>
+	<head>
+		<title><big>404</big></title>
+	</head>
+  		<body style =" background-color: #5882c4;">
+		<center>
+    		<h1 style="color: #27292d"><big><big><big><big><big><big><big><big>404 ERROR</big></big></big></big></big></big></big></big></h1>
+			<a href = "/"><img src="think.png" alt="Think" style="width:304px;height:304px;"></a>
+    		<p style="color: white"><big><big><big><big>This page doesn't exist yet.</big></big></big></big></p>
+  		</center>
+  	</body>
 </html>
