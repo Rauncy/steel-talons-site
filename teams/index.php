c08ce88bca8204f2849823f0d7c992e77cdbf43c
Blake Romero <blake.romero724@gmail.com>
2017-11-28 13:39:01 -0600

    Got search working, still not compiling on one page

--- teams/index.php
+++ teams/index.php
@@ -22,7 +22,7 @@
 			pageReload();
 		}
 	}
-	function pageData(pageNum) {
+	function pageData(pageNum,searchData) {
 		xhr = $.ajax({
 			url: 'https://www.thebluealliance.com/api/v3/teams/' + (pageNum-1),
 			headers: {
@@ -32,61 +32,106 @@
 			dataType: 'json',
 			success: function(data){
 				for (var i = 0; i < data.length; i++) {
-					if(data[i].nickname!=''&&data[i].nickname){
-						var team_name = data[i].nickname,
-								team_city = data[i].city,
-								team_number = data[i].team_number,
-								team_rook = data[i].rookie_year,
-								team_motto = data[i].motto,
-								team_state = data[i].state_prov,
-								team_country = data[i].country;
+					if(searchData!='')
+					{
+						if(data[i].nickname!=''&&data[i].nickname&&data[i].nickname.includes(searchData)){
+							var team_name = data[i].nickname,
+									team_city = data[i].city,
+									team_number = data[i].team_number,
+									team_rook = data[i].rookie_year,
+									team_motto = data[i].motto,
+									team_state = data[i].state_prov,
+									team_country = data[i].country;
 
-						//manually set conveniences
-						if(team_name.length>20){
-							team_name = team_name.substring(0,20)+"..."
+							//manually set conveniences
+							if(team_name.length>20){
+								team_name = team_name.substring(0,20)+"..."
+							}
+							if(team_number == "5427"){
+								team_motto = "Building a Legacy"
+							}
+							if(team_number == "7281"){
+								team_motto = "China numba one!"
+							}
+							if(team_motto!=null){
+								team_motto = team_motto.replace("'","");
+							}
+							var notAvailable = "N/A";
+							if(team_city == null){
+								team_city = notAvailable;
+							}
+							if(team_motto == null){
+								team_motto = notAvailable;
+							}
+							if(team_state == null){
+								team_state = notAvailable;
+							}
+							if(team_country == null){
+								team_country = notAvailable;
+							}
+							var $htmlCodeForTeam = '<a href="/teams/team_profile.php?team_number=' + team_number + '" title = "hovering">'+team_name+'</a> '+'- ('+team_number+')<br><br>';
+							$('#tba').append($htmlCodeForTeam);
 						}
-						if(team_number == "5427"){
-							team_motto = "Building a Legacy"
+					}
+					else {
+						if(data[i].nickname!=''&&data[i].nickname){
+							var team_name = data[i].nickname,
+									team_city = data[i].city,
+									team_number = data[i].team_number,
+									team_rook = data[i].rookie_year,
+									team_motto = data[i].motto,
+									team_state = data[i].state_prov,
+									team_country = data[i].country;
+
+							//manually set conveniences
+							if(team_name.length>20){
+								team_name = team_name.substring(0,20)+"..."
+							}
+							if(team_number == "5427"){
+								team_motto = "Building a Legacy"
+							}
+							if(team_number == "7281"){
+								team_motto = "China numba one!"
+							}
+							if(team_motto!=null){
+								team_motto = team_motto.replace("'","");
+							}
+							var notAvailable = "N/A";
+							if(team_city == null){
+								team_city = notAvailable;
+							}
+							if(team_motto == null){
+								team_motto = notAvailable;
+							}
+							if(team_state == null){
+								team_state = notAvailable;
+							}
+							if(team_country == null){
+								team_country = notAvailable;
+							}
+							var $htmlCodeForTeam = '<a href="/teams/team_profile.php?team_number=' + team_number + '" title = "hovering">'+team_name+'</a> '+'- ('+team_number+')<br><br>';
+							$('#tba').append($htmlCodeForTeam);
 						}
-						if(team_number == "7281"){
-							team_motto = "China numba one!"
-						}
-						if(team_motto!=null){
-							team_motto = team_motto.replace("'","");
-						}
-						var notAvailable = "N/A";
-						if(team_city == null){
-							team_city = notAvailable;
-						}
-						if(team_motto == null){
-							team_motto = notAvailable;
-						}
-						if(team_state == null){
-							team_state = notAvailable;
-						}
-						if(team_country == null){
-							team_country = notAvailable;
-						}
-						var $htmlCodeForTeam = '<a href="/teams/team_profile.php?team_number=' + team_number + '" title = "hovering">'+team_name+'</a> '+'- ('+team_number+')<br><br>';
-						$('#tba').append($htmlCodeForTeam);
 					}
 				}
 			}
 		})
 	}
 	function pageReload() {
-		document.getElementById('tba').innerHTML = ""
+		var url = new URL(window.location.href);
+		var searchData = url.searchParams.get("search");
+		document.getElementById('tba').innerHTML = "";
 		var pN = pageNum;
-		pageData(pageNum);
+		pageData(pageNum,searchData);
 		return false;
 	}
 	</script>
 	<h1 style = "display: inline;">The Blue Alliance Teams</h1>
 	<div style = "display: inline; margin-left: 500px; ">
-		<input id= "srch" type="text" name="search" onsubmit="pageReload()" placeholder="Search..">
+		<center><form onsumbit = "return false"><input id= "srch" type="text" name="search" onsubmit="pageReload()" placeholder="Search"></form>
 		<button type="button" name="prev" onclick="prev()">Prev</button>
 		<p style = "display: inline;padding: 20px;"id = "page">1</p>
-		<button type="button" name="next" onclick="next()">Next</button>
+		<button type="button" name="next" onclick="next()">Next</button></center>
 	</div>
 	<i style = "display: block"> --Click on Teams to Learn More--</i>
 	<div id="tba" style="column-count: 6;font-size:12px;padding: 40px;"></div>
