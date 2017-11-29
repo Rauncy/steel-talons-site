dfd07917855a5c7d946c52f8f630f263114033af
OTHSCS153 <OTHSCS153@KATYISD.ORG>
2017-11-29 13:39:44 -0600

    Updatet sessions

--- sessions.php
+++ sessions.php
@@ -1,6 +1,24 @@
 <?php
 //file name is first two digits of the hash
 class Session{
-  $ip
+  $ip;
+  $id;
+  $hash;
+
+  __construct($ip, $id, $hash){
+    $this->ip = $ip;
+    $this->id = $id;
+    $this->hash = $hash;
+  }
+}
+
+if(isset($GLOBALS['sessions']){
+
+}if(isset($GLOBALS['sessionHashes'])){
+
+}if(isset($GLOBALS['sessionIPs'])){
+
+}if(isset($GLOBALS['sessionIDs'])){
+
 }
 ?>
