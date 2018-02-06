3f1efa713c75ce40ed21d444cb4586d4fdbb1d3e
Akshat <respectthegame23@gmail.com>
2018-02-05 23:07:52 -0600

    made layout for pop-up profile card for each sponsor. TODO: add information to each

--- sponsors/index.php
+++ sponsors/index.php
@@ -1,19 +1,65 @@
 <?php $dir = ".."; include($dir . "/header.php"); ?>
 <link rel="stylesheet" type = "text/css" href=<?php echo $dir . "/css/sponsors.css" ?>>
 <h2 class = "info">Building a new robot every year and competing costs a lot of money.  Each regional competition costs the team $5,000 for first one and $4,000 for each additional one.  We are very thankful for all the organizations who have sponsored Tompkins Robotics in the past.</h2>
+<script defer>
+// When the user clicks on <div>, open the popup
+function pop(id) {
+    var popup = document.getElementById(id);
+    popup.classList.toggle("show");
+
+}
+</script>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 
 <h2 class="info"><u><strong>2018 Season Sponsors:</strong></u></h2>
 <div class = "sponsor_list">
-  <h3 class = "sponsor  first">Rockwell Automation</h3>
-  <h3 class = "sponsor">Texas Workforce Commission</h3>
-  <h3 class = "sponsor ">Finn Family & Philips66 Matching fund</h3>
-  <h3 class = "sponsor">Dupont Family</h3>
-  <h3 class = "sponsor ">The Hydrographic Society of America- HC</h3>
-  <h3 class = "sponsor">Best in Class Education Center</h3>
-  <h3 class = "sponsor ">  Espinosa Family & Exxon Mobil Matching  </h3>
-  <h3 class = "sponsor"> Marco's Pizza </h3>
-  <h3 class = "sponsor ">Forrest Orthodontics    </h3>
+  <h3 class = "sponsor  first" onclick="pop('rockwell')">Rockwell Automation<span class="popuptext" id="rockwell">
+    <div class="card">
+      <img src=<?php echo $dir . "/images/colin.jpg" ?> alt="John" width="70px;"style="width:100%">
+      <h1>John Doe</h1>
+      <p class="title">CEO & Founder, Example</p>
+      <p>Harvard University</p>
+      <a href="#"><i class="fa fa-dribbble"></i></a>
+      <a href="#"><i class="fa fa-twitter"></i></a>
+      <a href="#"><i class="fa fa-linkedin"></i></a>
+      <a href="#"><i class="fa fa-facebook"></i></a>
+      <p><button>Contact</button></p>
+    </div>
+  </span></h3>
+  <h3 class = "sponsor" onclick="pop('workforce')">Texas Workforce Commission
+    <span class="popuptext" id="workforce">
+      anything can be put in here (profile card)
+    </span></h3>
+  <h3 class = "sponsor " onclick="pop('finn')">Finn Family & Philips66 Matching Fund
+    <span class="popuptext" id="finn">
+      anything can be put in here (profile card)
+    </span></h3>
+  <h3 class = "sponsor" onclick="pop('dupont')">Dupont Family
+    <span class="popuptext" id="dupont">
+      anything can be put in here (profile card)
+    </span></h3>
+  <h3 class = "sponsor " onclick="pop('hydro')">The Hydrographic Society of America- HC
+    <span class="popuptext" id="hydro">
+      anything can be put in here (profile card)
+    </span></h3>
+  <h3 class = "sponsor" onclick="pop('best')">Best in Class Education Center
+    <span class="popuptext" id="best">
+      anything can be put in here (profile card)
+    </span></h3>
+  <h3 class = "sponsor " onclick="pop('espinosa')">  Espinosa Family & Exxon Mobil Matching
+    <span class="popuptext" id="espinosa">
+      anything can be put in here (profile card)
+    </span></h3>
+  <h3 class = "sponsor" onclick="pop('marco')"> Marco's Pizza
+    <span class="popuptext" id="marco">
+      anything can be put in here (profile card)
+    </span></h3>
+  <h3 class = "sponsor " onclick="pop('forrest')">Forrest Orthodontics
+    <span class="popuptext" id="forrest">
+      anything can be put in here (profile card)
+    </span></h3>
 </div>
 
 <br><br>
