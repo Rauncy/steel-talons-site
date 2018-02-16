d22698fb684c3b11375cd4f52119f705bf53a7c9
Akshat <respectthegame23@gmail.com>
2018-02-16 13:22:13 -0600

    team names is csv file, pls add to your database. TODO: display teams from databse rather than api pull, onclick go to a profile explaining the tea, getting info from a specific api pull

--- teams/index.php
+++ teams/index.php
@@ -2,8 +2,8 @@
 <body onload = "pageReload()">
 
 	<script type="text/javascript">
-	const upperBound = 17;
-	var pageNum = 1;
+	const upperBound = 15;
+	var pageNum = 0;
 	var xhr;
 	var currentTeamNumber=1;
 	function next(){
@@ -11,25 +11,25 @@
 		{
 			xhr.abort();
 			pageNum++;
-			document.getElementById('page').innerHTML = pageNum;
+			document.getElementById('page').innerHTML = pageNum+1;
 			pageReload();
 		}
 	}
 	function prev(){
-		if(pageNum>1)
+		if(pageNum>0)
 		{
 			xhr.abort();
 			pageNum--;
-			document.getElementById('page').innerHTML = pageNum;
+			document.getElementById('page').innerHTML = pageNum+1;
 			pageReload();
 		}
 	}
 	function pageData(pageNum,searchData) {
-		const RESULTLIMIT = 5;
+		const RESULTLIMIT = 15;
 		var resultCount=0;
-		 while(resultCount<RESULTLIMIT){
+		// while(resultCount<RESULTLIMIT){
 			xhr = $.ajax({
-			url: 'https://www.thebluealliance.com/api/v3/team/frc' +currentTeamNumber ,
+			url: 'https://www.thebluealliance.com/api/v3/teams/' + 14 ,
 			headers: {
 			'X-TBA-Auth-Key':'38wgMXShpksmFpPKfB8BLgT5kq8EajYkVlgnfT45FtL66TdI2agNuWllA8Nrzizx'
 			},
@@ -37,18 +37,22 @@
 			async: false,
 			dataType: 'json',
 			success: function(data){
+				for(var i =0; i<data.length;i++){
+				if(data[i]!=null){
 					if(!(searchData.length==0))
 					{
 						var searchRegex = new RegExp(searchData, 'i');
-						if(searchRegex.test(data.nickname) || searchData==data.team_number){
-							var team_name = data.nickname,
-									team_city = data.city,
-									team_number = data.team_number,
-									team_rook = data.rookie_year,
-									team_motto = data.motto,
-									team_state = data.state_prov,
-									team_country = data.country;
+						if(searchRegex.test(data[i].nickname) || searchData==data[i].team_number){
+							var team_name = data[i].nickname,
+									team_city = data[i].city,
+									team_number = data[i].team_number,
+									team_rook = data[i].rookie_year,
+									team_motto = data[i].motto,
+									team_state = data[i].state_prov,
+									team_country = data[i].country;
 							//manually set conveniences
+
+							var team_name_raw = team_name;
 							if(team_name.length>20){
 								team_name = team_name.substring(0,20)+"..."
 							}
@@ -74,20 +78,25 @@
 							if(team_country == null){
 								team_country = notAvailable;
 							}
-							var $htmlCodeForTeam = '<a href="/teams/team_profile.php?team_number=' + team_number + '" title = "hovering">'+team_name+'</a> '+'- ('+team_number+')<br><br>';
+							var $htmlCodeForTeam = '<a target = "_blank"  style = "font-size:22px;" id = "'+team_number+'" href="/teams/team_profile.php?team_number=' + team_number + '&team_name='+team_name_raw+'" title = "'+team_name_raw+'">'+team_name+'</a> '+'- ('+team_number+')<br><br>';
 							$('#tba').append($htmlCodeForTeam);
+							// var win = window.open('team_profile.php?team_number=' + team_number + '&team_name='+team_name_raw, '_blank');
+
+
 							resultCount++;
 						}
 					}
 					else {
-						if(data.nickname!=''&&data.nickname){
-							var team_name = data.nickname,
-									team_city = data.city,
-									team_number = data.team_number,
-									team_rook = data.rookie_year,
-									team_motto = data.motto,
-									team_state = data.state_prov,
-									team_country = data.country;
+						if(data[i].nickname!=''&&data[i].nickname){
+							var team_name = data[i].nickname,
+									team_city = data[i].city,
+									team_number = data[i].team_number,
+									team_rook = data[i].rookie_year,
+									team_motto = data[i].motto,
+									team_state = data[i].state_prov,
+									team_country = data[i].country;
+
+									var team_name_raw = team_name;
 							//manually set conveniences
 							if(team_name.length>20){
 								team_name = team_name.substring(0,20)+"..."
@@ -114,15 +123,20 @@
 							if(team_country == null){
 								team_country = notAvailable;
 							}
-							var $htmlCodeForTeam = '<a href="/teams/team_profile.php?team_number=' + team_number + '" title = "hovering">'+team_name+'</a> '+'- ('+team_number+')<br><br>';
+							var $htmlCodeForTeam = '<a target = "_blank" style = "font-size:22px;" id = "'+team_number+'"href="/teams/team_profile.php?team_number=' + team_number + '&team_name='+team_name_raw+'" title = "'+team_name_raw+'">'+team_name+'</a> '+'- ('+team_number+')<br><br>';
 							$('#tba').append($htmlCodeForTeam);
+							// if(team_number>1291)
+							 // var win = window.open('/teams/team_profile.php?team_number=' + team_number + '&team_name='+team_name_raw, '_blank');
+							 // win.close();
 							resultCount++;
 						}
 					}//else for checking if search data
 			}//ajax success
+		}//end for loop
+		}
 		})//end ajax request
 		currentTeamNumber++;
-	}//end while loop
+	//}//end while loop
 	}
 	function pageReload() {
 		var url = new URL(window.location.href);
@@ -142,4 +156,8 @@
 	</div>
 	<i style = "display: block"> --Click on Teams to Learn More--</i>
 	<div id="tba" style="column-count: 6;font-size:12px;padding: 40px;"></div>
+
+
+
+
 </body>
