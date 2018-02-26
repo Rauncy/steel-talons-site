ceeec998bee84dc5f73b360e050637e7d55cd2e4
Christopher R. Zeller <christopher.2927@gmail.com>
2018-02-25 21:53:12 -0600

    Organized logging in and out #New page logout #Updated header.php and .css #Register post moved to login and combined #I changed debug on index index

--- account/register.php
+++ account/register.php
@@ -1,25 +1,4 @@
 <?php
-function register(){
-  if(isset($_POST["registerUsername"]) && isset($_POST["registerEmail"])){
-    //Use database for user validation and creation
-    $servername = "localhost";
-    $username = "root";
-    $password = "admin";
-
-    $conn = new mysqli($servername, $username, $password, "robotics");
-
-    if($conn->connect_error){
-      die();
-    }
-
-    $result = $conn->query("select * from members where Username = \"" . $_POST["registerUsername"] . "\" or Email = \"" . $_POST["registerEmail"] . "\";");
-    if($result->num_rows == 0){
-      $conn->query("insert into members (Username, Email, Pass) values (\"" . $_POST['registerUsername'] . "\", \"" . $_POST['registerEmail'] . "\", \"" . $_POST['registerPassword'] . "\");");
-      //Post to login on new acct
-      header("Location: /account/login?loginEmail=".$_POST["registerEmail"]."&loginPassword=".$_POST["registerPassword"]."&submit=Login");
-    }
-  }
-}
 
 if(isset($_POST["registerUsername"])){
   switch($_POST["submit"]){
@@ -32,7 +11,7 @@ if(isset($_POST["registerUsername"])){
 <link rel = "stylesheet" href = "/css/login.css">
 <script src = "/js/account.js"></script>
 <h1 class="title">Register</h1>
-<form class="infoForm" onsubmit="register();" action="/account/register.php" method="post">
+<form class="infoForm" onsubmit="register();" action="/account/login.php" method="post">
   <center>
     <table>
       <tr>
