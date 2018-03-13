8e5b0f53a024422f8f79984099fb5eb614b07850
Akshat <respectthegame23@gmail.com>
2018-03-13 11:53:47 -0500

    normal members cant see everyone's password now. increased security for upcoming competition. developers can see it on MySQL. TODO: show password only for users with advanced permission. advanced permission can only be given by people who already have advanced permission. advanced permission will also allow people to edit information of members, scouting entries. NEED TO DO ALL THIS BY UPCOMING COMPETITION 3.16.18

--- members/index.php
+++ members/index.php
@@ -58,10 +58,10 @@ else {
 $result = $conn->query($sql);
 // echo $result;
 if ($result->num_rows > 0) {
-    echo "<table><tr><th>Name</th><th>Grade</th><th>Roles</th><th>Username</th><th>Password</th><th>Email</th><th>Phone Number</th></tr>";
+    echo "<table><tr><th>Name</th><th>Grade</th><th>Roles</th><th>Username</th><th>Email</th><th>Phone Number</th></tr>";
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "<tr><td>".$row["FirstName"]." ".$row["LastName"]."</td><td>".$row["Grade"]."</td><td>".$row["Roles"]."</td><td>".$row["Username"]."</td><td>".$row["Pass"]."</td><td>".$row["Email"].
+        echo "<tr><td>".$row["FirstName"]." ".$row["LastName"]."</td><td>".$row["Grade"]."</td><td>".$row["Roles"]."</td><td>".$row["Username"]."</td><td>".$row["Email"].
         "</td><td>(".substr($row["Phone"],0,3).") ".substr($row["Phone"],3,3)." - ".substr($row["Phone"],6,4)."</td></tr>";
     }
     echo "</table>";
