7e2c20720747cbab02cde46c973b30815739d685
Akshat <respectthegame23@gmail.com>
2018-03-15 08:51:06 -0500

    scouting form updated, commits to database

--- scouting/entries.php
+++ scouting/entries.php
@@ -30,11 +30,11 @@ $sql = "SELECT * FROM scouting";
 $result = $conn->query($sql);
 // echo $result;
 if ($result->num_rows > 0) {
-    echo "<table style = 'width: 100%;'><tr><th>FormID</th><th>Match Number</th><th>Team Number</th><th>Start Position</th><th>Abilities</th><th>Strategy</th><th>Switch</th><th>Scale</th><th>Vault</th><th>End Position</th><th>Climb Assist</th><th>Penalties</th><th>Notes</th><th>MemberID</th></tr>";
+    echo "<table style = 'width: 100%;'><tr><th>FormID</th><th>Match Number</th><th>Team Number</th><th>Start Position</th><th>Abilities</th><th>Strategy</th><th>Switch</th><th>Scale</th><th>Vault</th><th>End Position</th><th>Climb Assist</th><th>Penalties</th><th>Notes</th><th>Member</th></tr>";
     // output data of each row
     while($row = $result->fetch_assoc()) {
         echo "<tr><td>".$row["formID"]."</td><td>".$row["matchNumber"]."</td><td>".$row["teamNumber"]."</td><td>".$row["startPos"]."</td><td>".$row["abilities"]."</td><td>".$row["strategy"]."</td><td>".$row["switch"]."</td><td>".$row["scale"]
-        ."</td><td>".$row["vault"]."</td><td>".$row["endPos"]."</td><td>".$row["climbAsst"]."</td><td>".$row["penalties"]."</td><td>".$row["notes"]."</td><td>".$row["memberID"]."</td></tr>";
+        ."</td><td>".$row["vault"]."</td><td>".$row["endPos"]."</td><td>".$row["climbAsst"]."</td><td>".$row["penalties"]."</td><td>".$row["notes"]."</td><td>".$row["member"]."</td></tr>";
     }
     echo "</table>";
 } else {
