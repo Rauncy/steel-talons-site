b3702a27ad6f535319f9df305ee511c6a2b6d14e
Blake Romero <blake.romero724@gmail.com>
2018-04-12 22:14:34 -0500

    Made a thing for team things.

---
+++ teamsR/index.php
@@ -0,0 +1,36 @@
+<?php $dir = ".."; include($dir . "/header.php"); ?>
+<link rel = "stylesheet" href = "/css/teams.css">
+<center>
+  <h1 class = "title">Select a Team</h1>
+  <a href = "/teamsR/build.php">
+    <div>
+      Build
+    </div>
+  </a>
+  <a href = "/teamsR/programming.php">
+    <div>
+      Programming
+    </div>
+  </a>
+  <a href = "/teamsR/cad.php">
+    <div>
+      CAD
+    </div>
+  </a>
+  <a href = "/teamsR/marketing.php">
+    <div>
+      Marketing
+    </div>
+  </a>
+  <a href = "/teamsR/safety.php">
+    <div>
+      Safety
+    </div>
+  </a>
+  <a href = "/teamsR/scouting.php">
+    <div>
+      Scouting
+    </div>
+  </a>
+</center>
+<?php include($dir . "/footer.php") ?>
